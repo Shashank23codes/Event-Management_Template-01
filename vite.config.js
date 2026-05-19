a6bd1ba95b3623bca29769509600a4b346e5f716
Shashank23codes <shashankgupta4068@gmail.com>
2026-05-19 16:20:25 +0530

    feat: add gh-pages deployment scripts and configure base path for production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Event-Management_Template-01/',
   plugins: [
     react(),
     tailwindcss(),
